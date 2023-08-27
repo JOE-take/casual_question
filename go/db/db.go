b0d096f30faccd0a7fa637e8789e8b893db71354
joe <okoshi811@gmail.com>
2023-08-27 10:55:10 +0900

    fix: DB接続の際、何回かリトライするように

--- go/db/db.go
+++ go/db/db.go
@@ -4,18 +4,54 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"time"
+)
+
+const (
+	MaxRetry = 5
+	WaitTime = 5
 )
 
 func InitializeDB() *sql.DB {
 
-	db, err := sql.Open("mysql", "root:password@tcp(CQ-db:3306)/cq")
+	db, err := openDB()
 	if err != nil {
-		log.Fatal("failed to open database: ", err)
+		log.Fatal("failed to open database.\n", err)
 	}
 
-	if err := db.Ping(); err != nil {
-		log.Fatal("failed to connect database", err)
+	err = pingDB(db)
+	if err != nil {
+		log.Fatal("failed to connect database.\n", err)
 	}
 
 	return db
+}
+
+func openDB() (*sql.DB, error) {
+	var db *sql.DB
+	var err error
+
+	for i := 0; i <= MaxRetry; i++ {
+		db, err = sql.Open("mysql", "root:password@tcp(CQ-db:3306)/cq")
+		if err == nil {
+			break
+		}
+		time.Sleep(WaitTime * time.Second)
+	}
+
+	return db, err
+}
+
+func pingDB(db *sql.DB) error {
+	var err error
+
+	for i := 0; i <= MaxRetry; i++ {
+		err = db.Ping()
+		if err == nil {
+			break
+		}
+		time.Sleep(WaitTime * time.Second)
+	}
+
+	return err
 }
