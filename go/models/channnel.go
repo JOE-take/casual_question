bb931ea1f72e18215f0ed6f916dd3b9558e89273
joe <okoshi811@gmail.com>
2023-09-21 13:03:05 +0900

    feat: チャンネルと質問のモデルを追加

---
+++ go/models/channnel.go
@@ -0,0 +1,12 @@
+package models
+
+type Channel struct {
+	ChannelID string `db:"channel_id"`
+	Owner     string `db:"owner"`
+}
+
+type Question struct {
+	ID        string `db:"id"`
+	ChannelID string `db:"channel_id"`
+	Content   string `db:"content"`
+}
