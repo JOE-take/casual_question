bb1fa46804342bfb08a529681f98921f9bac8c59
joe <okoshi811@gmail.com>
2023-09-24 21:27:20 +0900

    feat: 期限切れのチャンネルとリフレッシュトークンを削除するルーチンを追加

--- go/main.go
+++ go/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"casual_question/db"
 	"casual_question/router"
+	"casual_question/routines"
 )
 
 func main() {
-	db := db.InitializeDB()
-	r := router.NewRouter(db)
+	dbCon := db.InitializeDB()
+	go routines.ControlRoutines(routines.RoutineInfo{DB: dbCon})
+	r := router.NewRouter(dbCon)
 	r.Run(":8080")
 }
