ea8ad54a984e12fadfb1a77dba586d69f11c7293
joe <okoshi811@gmail.com>
2023-09-24 23:33:02 +0900

    feat: Goのタイムゾーンを日本に設定

--- go/main.go
+++ go/main.go
@@ -4,10 +4,12 @@ import (
 	"casual_question/db"
 	"casual_question/router"
 	"casual_question/routines"
+	"time"
 )
 
 func main() {
 	dbCon := db.InitializeDB()
+	time.Local = time.FixedZone("Asia/Tokyo", 9*60*60)
 	go routines.ControlRoutines(routines.RoutineInfo{DB: dbCon})
 	r := router.NewRouter(dbCon)
 	r.Run(":8080")
