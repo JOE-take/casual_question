ea6ac75344a3e05956bc1cd82a0c88d794f74ec0
joe <okoshi811@gmail.com>
2023-09-25 18:36:29 +0900

    fix: Go側でタイムゾーンを変更しないように

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import (
 	"casual_question/db"
 	"casual_question/router"
 	"casual_question/routines"
-	"time"
 )
 
 func main() {
 	dbCon := db.InitializeDB()
-	time.Local = time.FixedZone("Asia/Tokyo", 9*60*60)
 	go routines.ControlRoutines(routines.RoutineInfo{DB: dbCon})
 	r := router.NewRouter(dbCon)
 	r.Run(":8080")
